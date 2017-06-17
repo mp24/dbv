0562ffddb047ff56aef6fb35cf447edd2faf6eab
Tina Neumann <tiny.nhuman@gmail.com>
2017-06-17 11:57:16 +0200

    erkennt schon FAST alle Schilder

--- src/dvbprojekt/QR_Plugin.java
+++ src/dvbprojekt/QR_Plugin.java
@@ -11,6 +11,7 @@ import ij.plugin.PlugIn;
 import ij.process.ImageConverter;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,15 +26,15 @@ public class QR_Plugin implements PlugIn {
     String path;
     ImagePlus original;
 
-    int minBoxHeight = 100;
-    int maxBoxHeight = 500;
+    int minBoxHeight = 70;
+    int maxBoxHeight = 350;// 500;
     int minBoxWidth = minBoxHeight / 2;
     int maxBoxWidth = maxBoxHeight;
     int maxAngle = 20;
 
     int scanColDist = 7;
-    
-    ArrayList<Roi> boundingBoxes = new ArrayList<Roi>();
+
+    ArrayList<Rectangle> boundingBoxes = new ArrayList<Rectangle>();
 
     @Override
     public void run(String string) {
@@ -46,25 +47,21 @@ public class QR_Plugin implements PlugIn {
             //original = IJ.getImage();
 
             //original = IJ.openImage("/home/tina/Desktop/IMG_20170530_102445.jpg");
-            original = IJ.openImage("/home/tina/Desktop/Screenshot from 2017-06-13 14:47:20.png");
-            //original = IJ.openImage("/home/tina/Desktop/Screenshot from 2017-06-13 14:48:32.png");
-            //original = IJ.openImage("/home/tina/Desktop/2.jpg");
+            //original = IJ.openImage("/home/tina/Desktop/Screenshot from 2017-06-13 14:47:20.png");
+            original = IJ.openImage("/home/tina/Desktop/Screenshot from 2017-06-13 14:48:32.png");
+            //original = IJ.openImage("/home/tina/Desktop/Screenshot from 2017-06-13 14:43:01.png");
         }
-        IJ.run(original, "Enhance Contrast...", "saturated=0.3 equalize");
-        //IJ.run(original, "Subtract Background...", "rolling=50 light separate");
-        ImagePlus bin = new ImagePlus("bin", original.getProcessor());
-        //IJ.run(bin, "Enhance Contrast...", "saturated=1.2 equalize");
-        IJ.run(bin, "Make Binary", "");
-        IJ.run(bin, "Invert", "");
-        ImageConverter ic = new ImageConverter(bin);
-        ic.convertToRGB();
 
-//        ImagePlus copie=new ImagePlus("copie",bin.getProcessor());
-//        ImageConverter ic2 = new ImageConverter(copie);
-//        ic2.convertToRGB();
-        //ArrayList<double[]> scans = new ArrayList();
+        ImagePlus bin = new ImagePlus("bin", original.getProcessor());
+        IJ.run(original, "Enhance Contrast...", "saturated=1.2 equalize");
+
+        IJ.run(bin, "8-bit", "");
+        IJ.setAutoThreshold(bin, "Otsu dark");
+        //IJ.setAutoThreshold(bin, "Minimum");
+        // IJ.setRawThreshold(bin, 100, 255, null);
+        IJ.run(bin, "Convert to Mask", "");
+
         HashMap<Integer, Line> segmentMap = new HashMap();
-      //int [] columnsXvals = new int[(bin.getWidth()/scanColDist)+1];
         int col = 0;
         int noSegments = 0;
         for (int x = 0; x < (bin.getWidth()); x = x + scanColDist) {
@@ -92,10 +89,9 @@ public class QR_Plugin implements PlugIn {
                 }
                 if (currSegmentHeight > minBoxHeight) {
                     segmentMap.put(noSegments, new Line(x, startY, x, startY + currSegmentHeight));
-                   // columnsXvals[col]=x;
                     //IJ.log("Column "+col+" Segment:"+noSegments+" höhe"+currSegmentHeight+" startY"+t);
-                    original.setColor(Color.magenta);
-                    original.getProcessor().drawLine(x, startY, x, startY + currSegmentHeight);
+//                    original.setColor(Color.magenta);
+//                    original.getProcessor().drawLine(x, startY, x, startY + currSegmentHeight);
                 }
             }
             col++;
@@ -104,24 +100,18 @@ public class QR_Plugin implements PlugIn {
         }
         IJ.log(segmentMap.toString());
         //IJ.log(Arrays.toString(columnsXvals));
-        int preavCol=0;
-        
+        int preavCol = 0;
+
         for (Iterator<Map.Entry<Integer, Line>> it = segmentMap.entrySet().iterator(); it.hasNext();) {
-            Map.Entry<Integer, Line> entry = it.next();       
+            Map.Entry<Integer, Line> entry = it.next();
             Line lineSeg = entry.getValue();
-              preavCol = lineSeg.x1; 
+            preavCol = lineSeg.x1;
             //IJ.log(entry.getKey() + "");
             if (lineSeg.y2 > (lineSeg.y1) + maxBoxHeight) { //entferne zu hohe linien  
-                original.setColor(Color.gray);
-                original.getProcessor().draw(lineSeg);
+//                original.setColor(Color.gray);
+//                original.getProcessor().draw(lineSeg);
                 it.remove();
             }
-//            else if(preavCol == (lineSeg.x1)-1){// segment in Zeile davor && dannach? 
-//                original.setColor(Color.yellow);
-//                original.getProcessor().draw(lineSeg);
-//                it.remove();
-//            }
-//            
         }
 
         IJ.log(segmentMap.toString());
@@ -129,24 +119,23 @@ public class QR_Plugin implements PlugIn {
         //key = segmentNumber
         for (Map.Entry<Integer, Line> segA : segmentMap.entrySet()) {
             for (Map.Entry<Integer, Line> segB : segmentMap.entrySet()) {
-              //for (Iterator<Map.Entry<Integer, Line>> it = segmentMap.entrySet().iterator(); it.hasNext();) {
+                //for (Iterator<Map.Entry<Integer, Line>> it = segmentMap.entrySet().iterator(); it.hasNext();) {
                 Line lA = segA.getValue();
                 Line lB = segB.getValue();
-                if (segA.getKey() != segB.getKey() && lA.x1 != lB.x1 ) { //Ungleiches Segment && UNgleiche Spalte?
-                    //  if(lB.y1 >= lA.y1+minBoxWidth && lB.y1 <= lA.y1+minBoxWidth){ //horizontale min & max distance?
+                if (segA.getKey() != segB.getKey() && lA.x1 != lB.x1) { //Ungleiches Segment && UNgleiche Spalte?
                     if (lB.x1 >= lA.x1 + minBoxWidth && lB.x1 <= lA.x1 + maxBoxWidth) { //horizontale min & max distance?
 
                         //sind segmente verbundne?
-                        int scanlineOffset = scanColDist / 2;
+                        int scanlineHOffset = scanColDist / 2;
                         //oben
                         boolean topConnected = false;
                         Line topHline = new Line(lA.x1, lA.y1, lB.x1, lB.y1);
-                        bin.setRoi( new Line(lA.x1, lA.y1+scanlineOffset, lB.x1, lB.y1+scanlineOffset));
+                        bin.setRoi(new Line(lA.x1, lA.y1 + scanlineHOffset, lB.x1, lB.y1 + scanlineHOffset));
                         if (topHline.getLength() <= lA.getLength()
                                 && topHline.getAngle() > (-maxAngle) && topHline.getAngle() < maxAngle) {
                             ProfilePlot topP = new ProfilePlot(bin);
                             double[] topProfile = topP.getProfile();
-                             //IJ.log(Arrays.toString(topProfile));
+                            //IJ.log(Arrays.toString(topProfile));
                             for (int t = 0; t < topProfile.length; t++) {
                                 if (topProfile[t] == 255) {
                                     topConnected = true;
@@ -161,7 +150,7 @@ public class QR_Plugin implements PlugIn {
                         //unten
                         boolean bottomConnected = false;
                         Line bottomHline = new Line(lA.x2, lA.y2, lB.x2, lB.y2);
-                        bin.setRoi(new Line(lA.x2, lA.y2-scanlineOffset, lB.x2, lB.y2-scanlineOffset));
+                        bin.setRoi(new Line(lA.x2, lA.y2 - scanlineHOffset, lB.x2, lB.y2 - scanlineHOffset));
                         if (bottomHline.getLength() <= lA.getLength()
                                 && bottomHline.getAngle() > (-maxAngle) && bottomHline.getAngle() < maxAngle) {
                             ProfilePlot bottomP = new ProfilePlot(bin);
@@ -179,57 +168,99 @@ public class QR_Plugin implements PlugIn {
                             // IJ.log(bottomHline.getAngle()+"");
                         }
 
-                        if (topConnected && bottomConnected ){//&& !alreadyInBox(lA, lB)) {
+                        if (topConnected && bottomConnected) {//&& !alreadyInBox(lA, lB)) {
 
-                            boundingBoxes.add(new Roi(lA.x1, lA.y1, topHline.getLength(), lA.getLength()));
-                            original.setColor(Color.green);
-                            // original.getProcessor().setFont(new Font("SansSerif", Font.PLAIN, 20));
-                            // original.getProcessor().drawString(segA.getKey() + "," + segB.getKey(), lA.x1, lB.y1);
-                            
-                            
-//                            original.getProcessor().draw(lA);
-//                            original.getProcessor().draw(lB);
+                            int scanlineVOffset = (int) (scanColDist * 1.8);
+
+                            boolean blackLeft = false;
+                            Line leftVline = new Line(lA.x1 + scanlineVOffset, lA.y1 + scanlineVOffset, lA.x2 + scanlineVOffset, lA.y2 - scanlineVOffset);
+                            bin.setRoi(leftVline);
+
+                            ProfilePlot leftP = new ProfilePlot(bin);
+                            double[] leftProfile = leftP.getProfile();
+                            for (int b = 0; b < leftProfile.length; b++) {
+                                //if () {
+                                    if (leftProfile[b] == 0) {
+                                        original.setColor(Color.red);
+                                        original.getProcessor().draw(leftVline);
+                                        blackLeft = true;
+                                    } else {
+                                        blackLeft = false;
+                                    }
+
+                               // }
+
+                            }
+
+                            boolean blackRight = false;
+                            Line rightVline = new Line(lB.x1 - scanlineVOffset, lB.y1 + scanlineVOffset, lB.x2 - scanlineVOffset, lB.y2 - scanlineVOffset);
+                            bin.setRoi(rightVline);
+
+                            ProfilePlot rightP = new ProfilePlot(bin);
+                            double[] rightProfile = rightP.getProfile();
+                            for (int b = 0; b < rightProfile.length; b++) {
+                                if (rightProfile[b] == 0) {
+                                    blackRight = true;
+                                    original.setColor(Color.green);
+                                    original.getProcessor().draw(rightVline);
+                                } else {
+                                    blackRight = false;
+                                }
+                            }
+                            if (blackLeft && blackRight) {
+                                boundingBoxes.add(new Rectangle((int) lA.x1, (int) lA.y1, (int) topHline.getLength(), (int) lA.getLength()));
+                            }
+//                                original.setColor(Color.green);
+//                                // original.getProcessor().setFont(new Font("SansSerif", Font.PLAIN, 20));
+//                                // original.getProcessor().drawString(segA.getKey() + "," + segB.getKey(), lA.x1, lB.y1);
 //
-                            original.setColor(Color.red);
-                            original.getProcessor().draw(topHline);
+//                                original.getProcessor().draw(lA);
+//                                original.getProcessor().draw(lB);
+////
+//                                original.setColor(Color.red);
+//                                original.getProcessor().draw(topHline);
+//
+//                                original.setColor(Color.blue);
+//                                original.getProcessor().draw(bottomHline);
 
-                            original.setColor(Color.blue);
-                            original.getProcessor().draw(bottomHline);
-                            
                         }
 
                     }
+
                 }
             }
+        }
+
+//         List<Rectangle> deleteCandidates = new ArrayList<>();
+//        for(Rectangle r1 :boundingBoxes){
+//           for(Rectangle r2 :boundingBoxes){
+//               if(!r1.equals(r2)){
+//                   if(r1.contains(r2)){
+//                      // boundingBoxes.remove(r2);
+//                       deleteCandidates.add(r2);
+//                   }else if(r2.contains(r1)){
+//                       //boundingBoxes.remove(r1);
+//                        deleteCandidates.add(r1);
+//                   }
+//                   
+//               }
+//            } 
+//        }
+//        for (Rectangle deleteCandidate : deleteCandidates) {
+//            boundingBoxes.remove(deleteCandidate);
+//         }
+        for (Rectangle r1 : boundingBoxes) {
+            original.setColor(Color.magenta);
+            original.getProcessor().draw(new Roi(r1));
         }
 
         original.show();
         bin.show();
     }
-    
-            private boolean inBetween(Line left, Line right, Line between) {
-//    private boolean alreadyInBox(int[] Xs, int[] Ys) { 
 
-            if (between.x1>left.x1 && right.x1>between.x1)               
-            {
-                //IJ.log("already in List (Code)");
-                return true;
-            }else{    
-                return false;
-            }
-
-    }
-        private boolean alreadyInBox(int x, int y) {
-//    private boolean alreadyInBox(int[] Xs, int[] Ys) {
-        for (Roi r : boundingBoxes) {
-            if (r.contains(x, y)) {
-                //IJ.log("already in List (Code)");
-                return true;
-            };
-        }
-        return false;
-    }
-
+//    private boolean conectedH(int x1, int y1, int x2, int y2){
+//    
+//    }
     /**
      * Main method for debugging.
      *
